6e4c36015644cf7994caf96429e0444769963694
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-06-07 18:35:23 -0500

    Tweaked damage numbers timing

--- DamageNumbers.gd
+++ DamageNumbers.gd
@@ -7,7 +7,7 @@ onready var target_position: Position2D = $Position2D
 
 export(Color) var heal_color: Color
 
-export(float) var float_duration_seconds: float = 0.65
+export(float) var float_duration_seconds: float = 1
 
 
 func play(value: int) -> void:
@@ -28,18 +28,23 @@ func play(value: int) -> void:
 				Tween.TRANS_LINEAR)
 	
 	_error = tween.interpolate_property(self, "scale",
-				Vector2.ZERO, scale,
-				float_duration_seconds * 0.5,
-				Tween.TRANS_BACK,
+				Vector2(0.3, 0.3), scale,
+				float_duration_seconds * 0.4,
+				Tween.TRANS_SINE,
 				Tween.EASE_OUT)
-	
-	_error = tween.interpolate_property(self, "modulate",
-				label.modulate, Color.transparent,
-				float_duration_seconds * 0.95,
-				Tween.TRANS_LINEAR)
 	
 	_error = tween.start()
 
 
 func _on_Tween_tween_all_completed() -> void:
 	queue_free()
+
+
+func _on_Tween_tween_completed(_object: Object, key: String) -> void:
+	if key == ":scale":
+		var _error = tween.interpolate_property(self, "modulate",
+					label.modulate, Color.transparent,
+					float_duration_seconds * 0.2,
+					Tween.TRANS_LINEAR)
+		
+		_error = tween.start()
