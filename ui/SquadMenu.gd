09a0f8141c9335b189b01cef7ff671e26a5aec66
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-06-11 21:30:12 -0500

    Active squad can now be viewed in the squad menu

--- ui/SquadMenu.gd
+++ ui/SquadMenu.gd
@@ -1,21 +1,38 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(PackedScene) var unit_item_packed_scene: PackedScene
 
+onready var list_container: VBoxContainer = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	for job in GameData.save_data.jobs:
-		print("job %s" % job.job_name)
-		pass
+	var save_data: SaveData = GameData.save_data
+	
+	for child in list_container.get_children():
+		child.queue_free()
+	
+	for index in save_data.active_units:
+		var job: Job = save_data.jobs[index]
+		
+		var unit_item: Control = unit_item_packed_scene.instance()
+		
+		list_container.add_child(unit_item)
+		
+		unit_item.initialize(job)
+		
+		unit_item.connect("change_button_clicked", self, "_on_UnitItem_change_button_clicked", [job, index])
+		unit_item.connect("view_button_clicked", self, "_on_UnitItem_view_button_clicked", [job, index])
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_UnitItem_change_button_clicked(job: Job, index: int) -> void:
+	# TODO: navigate
+	pass
+
+
+func _on_UnitItem_view_button_clicked(job: Job, index: int) -> void:
+	# TODO: Show unit view
+	# And reuse this screen for viewing enemy stats during battle
+	print("view")
 
 
 func _on_ReturnButton_pressed() -> void:
---
+++ ui/UnitItemContainer.gd
@@ -0,0 +1,22 @@
+extends HBoxContainer
+
+
+onready var name_label: Label = $VBoxContainer/Label
+onready var texture_rect: TextureRect = $NinePatchRect/TextureRect
+
+
+signal view_button_clicked
+signal change_button_clicked
+
+
+func initialize(job: Job) -> void:
+	name_label.text = tr(job.job_name)
+	texture_rect.texture = job.portrait
+
+
+func _on_ChangeButton_pressed() -> void:
+	emit_signal("change_button_clicked")
+
+
+func _on_ViewButton_pressed() -> void:
+	emit_signal("view_button_clicked")
