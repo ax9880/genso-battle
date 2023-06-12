2591bd983ff929c54d5048d811b66285603c3f9d
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-06-11 21:01:13 -0500

    Dialogue cutscene only starts showing text after scrolling has finished (actually just a bit before, so it looks better, using a timer)

--- ui/DialogueCutscene.gd
+++ ui/DialogueCutscene.gd
@@ -1,10 +1,6 @@
 extends Control
 
 
-#export(Array, Resource) var lines: Array
-#export(Array, PackedScene) var scenes
-
-#var scenes: Dictionary
 export(PackedScene) var dialogue_message_container_packed_scene
 
 onready var messages_container: VBoxContainer = $MarginContainer/VBoxContainer/ScrollContainer/MessagesVBoxContainer
@@ -12,9 +8,9 @@ onready var scroll_container: ScrollContainer = $MarginContainer/VBoxContainer/S
 
 # TODO: Load from a JSON?
 var lines: Array = [
-	{"speaker": "YACHIE", "line": "Hello, I am Yachie"},
-	{"speaker": "SAKI", "line": "Hello, Yachie, I am Saki. How are you in this fine day?"},
-	{"speaker": "YACHIE", "line": "Eh, been better. Have you seen Yuuma around?\n\nShe owes me 10 bucks"},
+	{"speaker": "YACHIE", "line": "Hello, I am Yachie."},
+	{"speaker": "SAKI", "line": "Hello, Yachie, I am Saki. How have you been?"},
+	{"speaker": "YACHIE", "line": "Eh, been better. Have you seen Yuuma around?\n\nShe owes me 10 bucks."},
 	{"speaker": "YUUMA", "line": "What's up gamers?"},
 	{"speaker": "SAKI", "line": "There she is."},
 	{"speaker": "YACHIE", "line": "Yuuma, I want my money back."},
@@ -23,6 +19,7 @@ var lines: Array = [
 
 var current_line: int = 0
 var current_dialogue_message_container: Control
+var estimated_container_size: float
 
 
 # Called when the node enters the scene tree for the first time.
@@ -41,10 +38,13 @@ func _show_next_line() -> void:
 	
 	current_dialogue_message_container.initialize(dialogue_message)
 	
-	# https://ask.godotengine.org/65100/scrollcontainer-autoscroll
-	yield(get_tree(), "idle_frame")
+	call_deferred("update_scroll")
+
+
+func update_scroll() -> void:
+	estimated_container_size += current_dialogue_message_container.rect_size.y
 	
-	if scroll_container.scroll_vertical != scroll_container.get_v_scrollbar().max_value:
+	if estimated_container_size > scroll_container.rect_size.y:
 		$Tween.interpolate_property(scroll_container, "scroll_vertical",
 			scroll_container.scroll_vertical, scroll_container.get_v_scrollbar().max_value,
 			1.0,
@@ -52,9 +52,9 @@ func _show_next_line() -> void:
 		
 		$Tween.start()
 		
-		yield($Tween, "tween_all_completed")
-	
-	current_dialogue_message_container.start_showing_text()
+		$Timer.start()
+	else:
+		current_dialogue_message_container.start_showing_text()
 
 
 func _input(event: InputEvent) -> void:
@@ -89,4 +89,12 @@ func _free_container_children() -> void:
 
 
 func _skip_dialogue() -> void:
-	get_tree().change_scene("res://ui/PreBattleMenu.tscn")
+	var _error = get_tree().change_scene("res://ui/PreBattleMenu.tscn")
+
+
+func _on_Timer_timeout() -> void:
+	current_dialogue_message_container.start_showing_text()
+
+
+func _on_SkipButton_pressed() -> void:
+	_skip_dialogue()
