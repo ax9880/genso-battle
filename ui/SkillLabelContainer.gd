695c5352d51b70fe1278424cc976911243ea33d6
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-06-12 21:18:24 -0500

    Skill label now prints skill name with area of effect and its size. Added more translations

--- ui/SkillLabelContainer.gd
+++ ui/SkillLabelContainer.gd
@@ -2,8 +2,14 @@ extends HBoxContainer
 
 
 func initialize(skill: Skill) -> void:
-	$Label.text = tr(skill.skill_name)
+	# The translation keys are the names of the enum values
+	var area_of_effect_translation_key: String = Enums.AreaOfEffect.keys()[skill.area_of_effect]
 	
-	# TODO: Add area of effect
+	var skill_description: String = tr(skill.skill_name) + ", " + tr(area_of_effect_translation_key)
+	
+	if not skill.is_targeted_individually():
+		skill_description += " (%d)" % skill.area_of_effect_size
+	
+	$Label.text = skill_description
 	
 	$TextureRect.texture = load(Enums.WEAPON_TYPE_TEXTURES[skill.primary_weapon_type])
