e773f9b77aa926ced07ed217c993bd22469af1c7
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-06-13 08:41:50 -0500

    Moved battle scene, created test battles, connected buttons in pre battle menu to navigate to test battles

--- ui/PreBattleMenu.gd
+++ ui/PreBattleMenu.gd
@@ -14,3 +14,10 @@ func _on_SquadButton_pressed() -> void:
 func _on_QuitButton_pressed() -> void:
 	var _error = get_tree().change_scene("res://ui/TitleScreen.tscn")
 
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://battles/TestBattle1.tscn")
+
+
+func _on_Button6_pressed():
+	get_tree().change_scene("res://battles/TestBattle2.tscn")
