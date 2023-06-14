0b4de653abaf3ca0cfd84decb947f5554d169d75
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-06-13 20:03:23 -0500

    Changed Trail2D for a node that holds an antialiased line 2D

--- Trail2D.gd
+++ Trail2D.gd
@@ -1,4 +1,4 @@
-extends Line2D
+extends Node2D
 
 export(int) var max_point_count: int = 20
 export(int) var interpolation_steps: int = 10
@@ -7,29 +7,31 @@ var last_stored_cell_point: Vector2
 
 var can_remove_faster: bool = false
 
+onready var line_2d: Line2D = $AntialiasedLine2D
+
 
 func _physics_process(delta):
-	if get_point_count() > max_point_count or (get_point_count() > 0 and can_remove_faster):
-		remove_point(0)
+	if line_2d.get_point_count() > max_point_count or (line_2d.get_point_count() > 0 and can_remove_faster):
+		line_2d.remove_point(0)
 
 
 func add(point: Vector2) -> void:
 	if not last_stored_cell_point.is_equal_approx(point):
-		if get_point_count() > 0:
+		if line_2d.get_point_count() > 0:
 			for i in range(1, interpolation_steps):
 				var weight: float = float(i) / float(interpolation_steps)
 				
 				var interpolated_point := last_stored_cell_point.linear_interpolate(point, weight)
 				
-				add_point(interpolated_point)
+				line_2d.add_point(interpolated_point)
 		
-		add_point(point)
+		line_2d.add_point(point)
 		
 		last_stored_cell_point = point
 	
-	if get_point_count() > max_point_count:
-		for i in range(get_point_count() - max_point_count - 5):
-			remove_point(0)
+	if line_2d.get_point_count() > max_point_count:
+		for i in range(line_2d.get_point_count() - max_point_count - 5):
+			line_2d.remove_point(0)
 	
 	if $RemovalStartTimer.is_stopped():
 		$RemovalStartTimer.start()
@@ -38,7 +40,7 @@ func add(point: Vector2) -> void:
 
 
 func clear() -> void:
-	clear_points()
+	line_2d.clear_points()
 
 
 func _on_RemovalStartTimer_timeout() -> void:
