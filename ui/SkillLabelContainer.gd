324683f10f2ea1390a895d577a57b45f15dcc1af
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-11-10 21:59:44 -0500

    Added translations for weapon types and formatted skill descriptions. Healing description is different. Multiplying absorb rate by 100 in description

--- ui/SkillLabelContainer.gd
+++ ui/SkillLabelContainer.gd
@@ -1,11 +1,13 @@
 extends HBoxContainer
 
+const COMMA_AND_SPACE: String = ", "
+
 
 func initialize(skill: Skill, can_show_full_data: bool = false) -> void:
 	# The translation keys are the names of the enum values
 	var area_of_effect_translation_key: String = Enums.AreaOfEffect.keys()[skill.area_of_effect]
 	
-	var skill_description: String = tr(skill.skill_name) + ", " + tr(area_of_effect_translation_key)
+	var skill_description: String = tr(skill.skill_name) + ", " + tr(area_of_effect_translation_key).to_lower()
 	
 	if not skill.is_targeted_individually():
 		skill_description += " (%d)" % skill.area_of_effect_size
@@ -13,23 +15,27 @@ func initialize(skill: Skill, can_show_full_data: bool = false) -> void:
 	if can_show_full_data:
 		$Label.autowrap = true
 		
-		skill_description += ", %.0f%%" % (100.0 * skill.activation_rate)
+		skill_description += COMMA_AND_SPACE + "%.0f%%" % (100.0 * skill.activation_rate)
 		
-		var primary_weapon_type_translation_key: String = Enums.WeaponType.keys()[skill.primary_weapon_type]
-		skill_description += ", %.1fx %s damage" % [skill.primary_power, primary_weapon_type_translation_key]
+		if skill.is_attack():
+			var primary_weapon_type_translation_key: String = Enums.WeaponType.keys()[skill.primary_weapon_type]
+			
+			skill_description += COMMA_AND_SPACE + tr("PRIMARY_WEAPON_DAMAGE_DESCRIPTION") % [skill.primary_power, tr(primary_weapon_type_translation_key).to_lower()]
+		elif skill.is_healing():
+			skill_description += COMMA_AND_SPACE + tr("HEAL_DESCRIPTION") % skill.primary_power
 		
 		if skill.secondary_power > 0:
 			var secondary_weapon_type_translation_key: String = Enums.WeaponType.keys()[skill.secondary_weapon_type]
 			
-			skill_description += " (+%.1fx %s damage)" % [skill.secondary_power, secondary_weapon_type_translation_key]
+			skill_description += " " + tr("SECONDARY_WEAPON_DAMAGE_DESCRIPTION") % [skill.secondary_power, tr(secondary_weapon_type_translation_key).to_lower()]
 		
 		if skill.absorb_rate > 0:
-			skill_description += ", absorb %.0f%%" % (skill.absorb_rate)
+			skill_description += COMMA_AND_SPACE + tr("ABSORB_DESCRIPTION") % (skill.absorb_rate * 100.0)
 		
 		if skill.status_effect != Enums.StatusEffectType.NONE:
 			var status_effect_type_translation_key: String = Enums.StatusEffectType.keys()[skill.status_effect]
 			
-			skill_description += ", inflict %s for %d turns" % [status_effect_type_translation_key, skill.status_effect_duration_turns]
+			skill_description += COMMA_AND_SPACE + tr("STATUS_EFFECT_DESCRIPTION") % [tr(status_effect_type_translation_key).to_lower(), skill.status_effect_duration_turns]
 	
 	$Label.text = skill_description
 	
