5235cf780f1da5c14c4526cd600ed6a483c40a05
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-11-11 12:48:47 -0500

    Showing max HP healed in skill description
    In full description showing "Skill: description ..."

--- ui/SkillLabelContainer.gd
+++ ui/SkillLabelContainer.gd
@@ -7,7 +7,12 @@ func initialize(skill: Skill, can_show_full_data: bool = false) -> void:
 	# The translation keys are the names of the enum values
 	var area_of_effect_translation_key: String = Enums.AreaOfEffect.keys()[skill.area_of_effect]
 	
-	var skill_description: String = tr(skill.skill_name) + ", " + tr(area_of_effect_translation_key).to_lower()
+	var skill_description: String = tr(skill.skill_name)
+	
+	if can_show_full_data:
+		skill_description += ": " + tr(area_of_effect_translation_key)
+	else:
+		skill_description += ", " + tr(area_of_effect_translation_key).to_lower()
 	
 	if not skill.is_targeted_individually():
 		skill_description += " (%d)" % skill.area_of_effect_size
@@ -23,6 +28,8 @@ func initialize(skill: Skill, can_show_full_data: bool = false) -> void:
 			skill_description += COMMA_AND_SPACE + tr("PRIMARY_WEAPON_DAMAGE_DESCRIPTION") % [skill.primary_power, tr(primary_weapon_type_translation_key).to_lower()]
 		elif skill.is_healing():
 			skill_description += COMMA_AND_SPACE + tr("HEAL_DESCRIPTION") % skill.primary_power
+			
+			skill_description += " " + tr("MAX_HEAL_DESCRIPTION").to_lower() % skill.max_heal
 		
 		if skill.secondary_power > 0:
 			var secondary_weapon_type_translation_key: String = Enums.WeaponType.keys()[skill.secondary_weapon_type]
@@ -31,6 +38,8 @@ func initialize(skill: Skill, can_show_full_data: bool = false) -> void:
 		
 		if skill.absorb_rate > 0:
 			skill_description += COMMA_AND_SPACE + tr("ABSORB_DESCRIPTION") % (skill.absorb_rate * 100.0)
+			
+			skill_description += " " + tr("MAX_HEAL_DESCRIPTION").to_lower() % skill.max_heal
 		
 		if skill.status_effect != Enums.StatusEffectType.NONE:
 			var status_effect_type_translation_key: String = Enums.StatusEffectType.keys()[skill.status_effect]
