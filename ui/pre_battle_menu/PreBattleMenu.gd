49db42e2abdb897ceac0b145ff10d798c4de2080
ax9880 <25557476+ax9880@users.noreply.github.com>
2023-11-27 22:34:53 -0500

    Updated references to unlocked chapters in pre-battle menu

--- ui/pre_battle_menu/PreBattleMenu.gd
+++ ui/pre_battle_menu/PreBattleMenu.gd
@@ -5,7 +5,7 @@ export(PackedScene) var battle_button_packed_scene: PackedScene
 
 
 func _ready() -> void:
-	_create_buttons_for_unlocked_battles()
+	_create_buttons_for_unlocked_chapters()
 	
 	_set_focus()
 
@@ -20,20 +20,18 @@ func _set_focus() -> void:
 	$MarginContainer/VBoxContainer/HBoxContainer/SquadButton.grab_focus()
 
 
-func _create_buttons_for_unlocked_battles() -> void:
+func _create_buttons_for_unlocked_chapters() -> void:
 	for child in $MarginContainer/VBoxContainer/VBoxContainer2.get_children():
 		child.queue_free()
 	
 	var save_data: SaveData = GameData.save_data
 	
-	var unlocked_battles = save_data.unlocked_battles
-	
-	for unlocked_battle in save_data.unlocked_battles:
+	for unlocked_chapter in save_data.unlocked_chapters:
 		var button: Button = battle_button_packed_scene.instance()
 		
-		button.text = tr(unlocked_battle.title)
+		button.text = tr(unlocked_chapter.title)
 		
-		button.connect("pressed", self, "on_BattleButton_pressed", [unlocked_battle])
+		button.connect("pressed", self, "on_ChapterButton_pressed", [unlocked_chapter])
 		
 		$MarginContainer/VBoxContainer/VBoxContainer2.add_child(button)
 
@@ -46,8 +44,8 @@ func _on_QuitButton_pressed() -> void:
 	change_scene("res://ui/main_menu/StackBasedMainMenu.tscn")
 
 
-func on_BattleButton_pressed(unlocked_battle: UnlockedBattle) -> void:
-	var current_scene_path: String = unlocked_battle.current_scene
+func on_ChapterButton_pressed(unlocked_chapter: ChapterSaveData) -> void:
+	var current_scene_path: String = unlocked_chapter.current_scene
 	
 	if current_scene_path == null:
 		# TODO: Get path from constants or resource
