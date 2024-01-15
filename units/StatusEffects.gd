05c05f66b5bf73aa5f0e14f0009e57061e7c897c
ax9880 <25557476+ax9880@users.noreply.github.com>
2024-01-14 21:29:45 -0500

    Added status_effects_counter to not remove status effect scene from unit when stacked status effect is removed but unit still has other status effects of the same type

--- units/StatusEffects.gd
+++ units/StatusEffects.gd
@@ -3,6 +3,10 @@ extends Node2D
 # Dictionary<StatusEffectType, PackedScene>
 var status_effects := {}
 
+# Dictionary<StatusEffectType, int>
+# TODO: Use class to group PackedScene and int
+var status_effects_counter := {}
+
 
 func add(status_effect_type: int, effect_scene: PackedScene) -> void:
 	if not status_effects.has(status_effect_type):
@@ -11,13 +15,19 @@ func add(status_effect_type: int, effect_scene: PackedScene) -> void:
 		add_child(effect)
 		
 		status_effects[status_effect_type] = effect
+		status_effects_counter[status_effect_type] = 1
+	else:
+		status_effects_counter[status_effect_type] += 1
 
 
 func remove(status_effect_type: int) -> void:
-	var effect: Node2D = status_effects.get(status_effect_type)
+	status_effects_counter[status_effect_type] -= 1
 	
-	if effect != null:
-		effect.stop()
+	if status_effects_counter[status_effect_type] == 0:
+		var effect: Node2D = status_effects.get(status_effect_type)
+	
+		if effect != null:
+			effect.stop()
 		
 		if not status_effects.erase(status_effect_type):
 			print("Tried to erase unexisting status effect")
