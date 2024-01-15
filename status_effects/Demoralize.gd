5f57ceba91df6fde1d634974efcde0ab1a23f06e
ax9880 <25557476+ax9880@users.noreply.github.com>
2024-01-14 21:32:00 -0500

    Marked unused variable

--- status_effects/Demoralize.gd
+++ status_effects/Demoralize.gd
@@ -4,7 +4,7 @@ extends StatusEffect
 var random := RandomNumberGenerator.new()
 
 
-func modify_stats(base_stats: StartingStats, modified_stats: StartingStats) -> void:
+func modify_stats(_base_stats: StartingStats, modified_stats: StartingStats) -> void:
 	modified_stats.attack = 0
 	
 	modified_stats.skill_activation_rate_modifier = -1
