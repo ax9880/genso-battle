84e35c78a98e285874f4cf92e1c7f2f725924890
ax9880 <25557476+ax9880@users.noreply.github.com>
2024-01-24 20:07:24 -0500

    Condition always increases turn counter when turn condition is valid, even if the other checks don't pass, so that turn counter can eventually match

--- units/ai/Condition.gd
+++ units/ai/Condition.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 # At what HP percentage this condition is true
+# Setting 1.0 is equivalent to ignoring this value
 export(float, 0.1, 1, 0.1) var hp_percentage: float = 1.0
 
 # Whether this condition should only activate once
@@ -12,33 +13,43 @@ export(bool) var is_one_shot: bool = false
 export(bool) var can_check_turn_counter: bool = true
 
 # At which specific enemy turn this condition is true
-export(int) var turn_offset: int = 0
+export(int) var turn_offset: int = 1
 
 # This condition repeats every X turns
-# turn + turn_x * counter
+# turn_offset + turn_steps * counter
 export(int, 0, 20, 1) var turn_steps: int = 1
 
 # Set after a one-shot condition is activated and the corresponding action
 # is performed
-var is_activated: bool = false
+var _is_activated: bool = false
 
-var counter: int = 0
+# Turn counter, increased every time the turn check matches, even if the
+# rest of the checks do not pass.
+var _counter: int = 0
 
 
 func is_true(current_hp_percentage: float, current_turn: int, can_use_turn_counter: bool) -> bool:
+	var is_current_counter_valid: bool = _is_current_counter_valid(current_turn)
+	
+	if is_current_counter_valid:
+		# Keep track of the counter even if the condition is not fulfilled
+		_counter += 1
+	
 	if current_hp_percentage > hp_percentage:
 		return false
 	
-	if is_one_shot and is_activated:
+	if is_one_shot and _is_activated:
 		return false
 	
-	if can_use_turn_counter and can_check_turn_counter and (turn_offset + counter * turn_steps) != current_turn:
+	if can_use_turn_counter and can_check_turn_counter and not is_current_counter_valid:
 		return false
-	
-	counter += 1
 	
 	return true
 
 
 func reset_turn_counter() -> void:
-	counter = 0
+	_counter = 0
+
+
+func _is_current_counter_valid(current_turn: int) -> bool:
+	return turn_offset + (_counter * turn_steps) == current_turn
