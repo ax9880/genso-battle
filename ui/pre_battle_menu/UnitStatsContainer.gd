b440e5c3f873bf498d0d3f8d39aa7673e961a285
ax9880 <25557476+ax9880@users.noreply.github.com>
2024-02-07 19:49:20 -0500

    Increased unit item font size to 18. UnitStatsContainer now uses onready vars

--- ui/pre_battle_menu/UnitStatsContainer.gd
+++ ui/pre_battle_menu/UnitStatsContainer.gd
@@ -1,26 +1,33 @@
 extends VBoxContainer
 
+onready var health_number_label: Label = $HBoxContainer3/HealthNumber
+
+onready var attack_number_label: Label = $HBoxContainer/AttackNumber
+onready var defense_number_label: Label = $HBoxContainer/DefenseNumber
+onready var spiritual_attack_number_label: Label = $HBoxContainer2/SpiritualAttackNumber
+onready var spiritual_defense_number_label: Label = $HBoxContainer2/SpiritualDefenseNumber
+
 
 func initialize(base_stats: StartingStats, compare_stats: StartingStats, can_show_remaining_health: bool = false) -> void:
 	# TODO: Add label translations
 	
 	if compare_stats != null:
 		if not can_show_remaining_health:
-			_show_compared_number($HBoxContainer3/HealthNumber, base_stats.health, compare_stats.health)
+			_show_compared_number(health_number_label, base_stats.health, compare_stats.health)
 		
-		_show_compared_number($HBoxContainer/AttackNumber, base_stats.attack, compare_stats.attack)
-		_show_compared_number($HBoxContainer/DefenseNumber, base_stats.defense, compare_stats.defense)
+		_show_compared_number(attack_number_label, base_stats.attack, compare_stats.attack)
+		_show_compared_number(defense_number_label, base_stats.defense, compare_stats.defense)
 		
-		_show_compared_number($HBoxContainer2/SpiritualAttackNumber, base_stats.spiritual_attack, compare_stats.spiritual_attack)
-		_show_compared_number($HBoxContainer2/SpiritualDefenseNumber, base_stats.spiritual_defense, compare_stats.spiritual_defense)
+		_show_compared_number(spiritual_attack_number_label, base_stats.spiritual_attack, compare_stats.spiritual_attack)
+		_show_compared_number(spiritual_defense_number_label, base_stats.spiritual_defense, compare_stats.spiritual_defense)
 	else:
-		_show_number($HBoxContainer3/HealthNumber, base_stats.health)
+		_show_number(health_number_label, base_stats.health)
 		
-		_show_number($HBoxContainer/AttackNumber, base_stats.attack)
-		_show_number($HBoxContainer/DefenseNumber, base_stats.defense)
+		_show_number(attack_number_label, base_stats.attack)
+		_show_number(defense_number_label, base_stats.defense)
 		
-		_show_number($HBoxContainer2/SpiritualAttackNumber, base_stats.spiritual_attack)
-		_show_number($HBoxContainer2/SpiritualDefenseNumber, base_stats.spiritual_defense)
+		_show_number(spiritual_attack_number_label, base_stats.spiritual_attack)
+		_show_number(spiritual_defense_number_label, base_stats.spiritual_defense)
 	
 	if can_show_remaining_health and compare_stats != null:
 		_show_remaining_health(base_stats, compare_stats)
@@ -46,4 +53,4 @@ func _show_compared_number(label: Label, job_stat: int, compare_job_stat: int) -
 
 
 func _show_remaining_health(base_stats: StartingStats, current_stats: StartingStats) -> void:
-	$HBoxContainer3/HealthNumber.text = "%d/%d" % [current_stats.health, base_stats.health]
+	health_number_label.text = "%d/%d" % [current_stats.health, base_stats.health]
