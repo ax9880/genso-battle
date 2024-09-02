2fa7f3e25ef0a954f49c4b6e8ca435bd73203eff
ax9880 <25557476+ax9880@users.noreply.github.com>
2024-09-01 19:46:09 -0500

    Unlocking skills every 10 levels

--- jobs/Job.gd
+++ jobs/Job.gd
@@ -2,12 +2,14 @@ extends Resource
 
 class_name Job
 
+# Unlock skills every X levels
+const _UNLOCK_SKILL_LEVEL_MULTIPLE: int = 10
+
 # StartingStats
 # Has to be unique (duplicated)
 export(Resource) var stats: Resource = null
 
 # Array<Skill>
-# Assuming one skill per level
 export(Array, Resource) var skills: Array = []
 
 export(String) var job_name: String = ""
@@ -18,7 +20,9 @@ export(Texture) var full_portrait: Texture = null
 
 
 func get_unlocked_skills(level: int) -> Array:
-	if level <= 0:
+	var skills_unlocked_count: int = floor(level / _UNLOCK_SKILL_LEVEL_MULTIPLE)
+	
+	if skills_unlocked_count == 0:
 		return []
 	else:
-		return skills.slice(0, level - 1)
+		return skills.slice(0, skills_unlocked_count)
